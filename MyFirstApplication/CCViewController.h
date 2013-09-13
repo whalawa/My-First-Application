efa989e2eb477eec4a525ccf8ccbd8906e634bd1
Eliot Arntz <arntzel@gmail.com>
2013-09-13 18:22:46 +0200

    initial commit

--- MyFirstApplication/CCViewController.h
+++ MyFirstApplication/CCViewController.h
@@ -10,4 +10,11 @@
 
 @interface CCViewController : UIViewController
 
+//IBOutlets are references to view objects in our storyboard
+@property (strong, nonatomic) IBOutlet UITextField *textField;
+@property (strong, nonatomic) IBOutlet UILabel *titleLabel;
+
+//IBAction is a way for our view object to send a message to our Controller that the button was tapped.
+- (IBAction)buttonPressed:(id)sender;
+
 @end
--- MyFirstApplication/CCViewController.m
+++ MyFirstApplication/CCViewController.m
@@ -26,4 +26,15 @@
     // Dispose of any resources that can be recreated.
 }
 
+/*
+ This method was created when we control dragged from the story board and hooked up our button as an action.  Now when the user presses the button the code inside the curly braces will evaluate.
+ */
+
+- (IBAction)buttonPressed:(id)sender
+{
+    //We can update the text of our label by assigning the text property value of the textfield.
+    self.titleLabel.text = self.textField.text;
+    //Using the resignFirstResponder method, which is functionality created by Apple, we can tell the textField to hide the keyboard.
+    [self.textField resignFirstResponder];
+}
 @end
